4735b0f861451466b2fbee8cbfb7935e25c6d019
Dennis de Best <dennis@debest.fr>
2020-06-30 22:17:58 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM node:13-alpine
+
+# Installs latest Chromium package.
+RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" > /etc/apk/repositories \
+    && echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories \
+    && echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories \
+    && echo "http://dl-cdn.alpinelinux.org/alpine/v3.11/main" >> /etc/apk/repositories \
+    && apk upgrade -U -a \
+    && apk add --no-cache \
+    libstdc++ \
+    chromium \
+    harfbuzz \
+    nss \
+    freetype \
+    ttf-freefont \
+    wqy-zenhei \
+    bash \
+    && rm -rf /var/cache/* \
+    && mkdir /var/cache/apk
+
+
+RUN mkdir -p /usr/src/app
+
+WORKDIR /usr/src/app
+
+RUN wget https://raw.githubusercontent.com/jfrazelle/dotfiles/master/etc/docker/seccomp/chrome.json -P /usr/src
+
+RUN export CHROME_PATH=/usr/lib/chromium/
+
+RUN yarn global add lighthouse
+
+COPY audit.sh /usr/local/bin/audit
+RUN chmod +x /usr/local/bin/audit
---
+++ audit.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 [-u <string>] [-p <string>] [-o <string>] [-n <string>]" 1>&2; exit 1; }
+defaultOptions='--chrome-flags="--headless --no-sandbox" --no-enable-error-reporting'
+while getopts ":u:p:o:n:" o; do
+case "${o}" in
+  u)
+    url=${OPTARG}
+    ;;
+  p)
+    IFS=',' read -r -a paths <<< "${OPTARG}"
+    ;;
+  o)
+    defaultOptions=${OPTARG}
+    ;;
+  n)
+    name=${OPTARG}
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+esac
+done
+shift $((OPTIND-1))
+
+if [ -n "$url" ]; then
+  outputPath=""
+  if [ -n "$name" ];then
+    outputPath=" --output-path ./${name}.report.html"
+  fi
+  eval lighthouse "$url" "$defaultOptions" "$outputPath"
+  if [ -n "$url" ]; then
+    for path in "${paths[@]}"; do
+      if [ -n "$name" ];then
+        filename=$(echo "$name""$path" | tr / _)
+        outputPath=" --output-path ./${filename}.report.html"
+      fi
+      eval lighthouse "$url""$path" "$defaultOptions" "$outputPath"
+    done
+  fi
+fi
+
+
+
